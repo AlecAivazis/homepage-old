df72da566d5189399cbe2c7184fe51aeafdfd143
alec aivazis <alec@aivazis.com>
2015-04-04 20:00:02 -0700

    added tests for retrieving posts by their tag

--- homepage/blog/tests.py
+++ homepage/blog/tests.py
@@ -23,9 +23,12 @@ class PostTestCase(TestCase):
         """ Create the necessary models for the test case """
         self.first = Post.objects.create(title="first", body="first", post_date=timezone.now())
         self.second = Post.objects.create(title="second", body="second", post_date=timezone.now())
+        # add different tags to each
+        self.first.tags.add('first')
+        self.second.tags.add('second')
+        
 
-
-    def test_posts_retrieve_latest_first(self):
+    def test_filter_post_latest_first(self):
         """ verfiy that the latest post is first  """
         # get the latest post
         latest = Post.objects.latest_first()[0]
@@ -33,7 +36,7 @@ class PostTestCase(TestCase):
         self.assertEqual(latest, self.second)
 
 
-    def test_posts_retrieve_earliest_first(self):
+    def test_filter_post_earliest_first(self):
         """ test that the earliest post is the first """
         # get the earliest post
         earliest = Post.objects.oldest_first()[0]
@@ -41,12 +44,21 @@ class PostTestCase(TestCase):
         self.assertEqual(earliest, self.first)
 
 
+    def test_filter_post_by_tag(self):
+        """ test that we can retrieve posts by their tags """
+        # return the post that matches 
+        retrieved = Post.objects.with_tag('first')[0]
+        # verify that we got the first one
+        self.assertEqual(retrieved, self.first)
+
+
     def test_retrieve_by_slug(self):
-        """ that that we can retrieve a particular post by its slug """
+        """ test that we can retrieve a particular post by its slug """
         # retrieve the post that has the second's slug
-        retrieved = Post.objects.with_slug(self.second.slug)
+        retrieved = Post.objects.get_with_slug(self.second.slug)
         # verify that the two posts are the same
         self.assertEqual(retrieved, self.second)
+
 
 
 # end of file
