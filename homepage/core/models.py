e9bdf53980831572713fc1209c2fb623b59983f3
alec aivazis <alec@aivazis.com>
2015-05-10 20:31:01 -0700

    replaced project screenshot with a m2m relationship

--- homepage/core/models.py
+++ homepage/core/models.py
@@ -5,15 +5,13 @@
 #
 # this file describes the base models for homepage
 
-# import the django models
+# django imports
 from django.db import models
 
-# base models
 class Project(models.Model):
     title = models.CharField(max_length=1020)
     description = models.CharField(max_length=1020)
     preview = models.FileField()
-    screenshot = models.FileField()
     link = models.CharField(max_length=1020, blank=True)
 
     # definitions for type choices
@@ -27,6 +25,12 @@ class Project(models.Model):
     # the string behavior is to return the title
     def __str__(self):
         return self.title
+
+
+class ProjectScreenshot(models.Model):
+    """ The model for a scheen shot associated with a given project """
+    file = models.FileField()
+    project = models.ForeignKey(Project, related_name="screenshots")
 
 
 # end of file
